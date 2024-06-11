78225d2763678030dbe007b2885119b9c052a706
drmodun <jan.modun.st@gmail.com>
2024-06-11 18:22:50 +0200

    more setup

---
+++ apps/api/src/db/drizzle.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'drizzle-kit';
+import { env } from 'process';
+export default defineConfig({
+  dialect: 'postgresql',
+  schema: './schema.ts',
+  out: './drizzle',
+  dbCredentials: {
+    url:
+      env.DATABASE_URL ||
+      'postgresql://postgres:postgres@localhost:5432/postgres', //default
+  },
+});
